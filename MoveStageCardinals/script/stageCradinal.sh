f4ffd3e0da6cb6b3e1e95dd7b831d755c7fb9a34
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-14 18:21:05 +0530

    Update stageCradinal.sh

--- MoveStageCardinals/script/stageCradinal.sh
+++ MoveStageCardinals/script/stageCradinal.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-
 # Set file and directory paths
 cardinal_cost_file="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv"
 source_directory="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/source_directory"
-
 # Move Cardinal cost file to source directory
 echo "Moving Cardinal cost file to source directory..."
 mv "$cardinal_cost_file" "$source_directory"
-
 # Set database connection details
 central_db_host="localhost"
 central_db_user="root"
 central_db_password="priya"
 central_db_name="EPRN_Central "
-
 # Load product cost file into the EPRN Central database
 echo "Loading product cost file into the EPRN Central database..."
 mysql -h $central_db_host -u $central_db_user -p$central_db_password --local-infile=1 $central_db_name << EOF
@@ -33,5 +29,4 @@ UPDATE Product_Store AS sp
 JOIN Product_Cardinal AS pc ON sp.Product_ID = pc.Product_ID
 SET sp.Price = pc.Price;
 EOF
-
 echo "Cost changes pushed to the store database."
