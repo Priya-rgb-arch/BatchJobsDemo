88aaac32e5eb1966f313703572d928db1da276ea
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-14 18:24:38 +0530

    Update stageCradinal.sh

--- MoveStageCardinals/script/stageCradinal.sh
+++ MoveStageCardinals/script/stageCradinal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Set file and directory paths
-cardinal_cost_file="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv"
-source_directory="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/source_directory"
+cardinal_cost_file="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv" 
+source_directory="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/source_directory/"
 # Move Cardinal cost file to source directory
 echo "Moving Cardinal cost file to source directory..."
 mv "$cardinal_cost_file" "$source_directory"
