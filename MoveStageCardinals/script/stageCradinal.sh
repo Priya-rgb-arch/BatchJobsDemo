119aa07c244e9833cb1e0f16a454d5c84a06d957
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-14 16:10:09 +0530

    Update stageCradinal.sh

--- MoveStageCardinals/script/stageCradinal.sh
+++ MoveStageCardinals/script/stageCradinal.sh
@@ -4,20 +4,15 @@
 cardinal_cost_file="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv"
 source_directory="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/source_directory"
 
+# Move Cardinal cost file to source directory
+echo "Moving Cardinal cost file to source directory..."
+mv "$cardinal_cost_file" "$source_directory"
+
 # Set database connection details
 central_db_host="localhost"
 central_db_user="root"
 central_db_password="priya"
 central_db_name="EPRN_Central "
-
-store_db_host="localhost"
-store_db_user="root"
-store_db_password="priya"
-store_db_name="store_DB"
-
-# Move Cardinal cost file to source directory
-echo "Moving Cardinal cost file to source directory..."
-mv "$cardinal_cost_file" "$source_directory"
 
 # Load product cost file into the EPRN Central database
 echo "Loading product cost file into the EPRN Central database..."
@@ -27,6 +22,13 @@ INTO TABLE Product_Cardinal
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 IGNORE 1 LINES;
 EOF
+
+
+store_db_host="localhost"
+store_db_user="root"
+store_db_password="priya"
+store_db_name="store_DB"
+
 
 # Push cost changes to the store database
 echo "Pushing cost changes to the store database..."
