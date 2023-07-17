2bf26138c12117cc1bdfb7427a7a183ab5286c67
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-17 17:30:02 +0530

    Update stageCradinal.sh

--- MoveStageCardinals/script/stageCradinal.sh
+++ MoveStageCardinals/script/stageCradinal.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
-# Set file and directory paths
-cardinal_cost_file="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv"
-#source_directory="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/source_directory/"
-# Move Cardinal cost file to source directory
-#echo "Moving Cardinal cost file to source directory..."
-#mv "/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv" "$source_directory"
-# Set database connection details
-central_db_host="localhost"
-central_db_user="root"
-central_db_password="priya"
-central_db_name="EPRN_Central "
+
+# Database store connection details
+db_host="localhost"
+db_user="root"
+db_password="priya"
+db_store="EPRN_Central"
+
+
+# CSV file path and name
+csv_file="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv"
+
 # Database table name
-central_table_name="Product_Cardinal"
-# Load product cost file into the EPRN Central database
-echo "Loading product cost file into the EPRN Central database..."
-mysql -h $central_db_host -u $central_db_user -p$central_db_password --local-infile=1 $central_db_name << EOF
-LOAD DATA INFILE '$cardinal_cost_file'
-INTO TABLE $central_table_name
+store_table_name="Product_Cardinal"
+
+
+# Read the CSV file and insert data into the database table
+mysql -h "$db_host" -u "$db_user" -p"$db_password" "$db_store" <<EOF
+LOAD DATA LOCAL INFILE '$csv_file'
+INTO TABLE $store_table_name
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 EOF
-echo "Cost changes pushed to the store database."
+
+if [ $? -eq 0 ]; then
+  echo "Data inserted into the database table successfully."
+else
+  echo "Error inserting data into the database table."
+fi
