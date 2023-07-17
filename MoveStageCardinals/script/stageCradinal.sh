4f594c10f7c434f1c2664313880e47474172fc64
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-17 17:18:46 +0530

    Update stageCradinal.sh

--- MoveStageCardinals/script/stageCradinal.sh
+++ MoveStageCardinals/script/stageCradinal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Set file and directory paths
-#cardinal_cost_file="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv"
-source_directory="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/source_directory/"
+cardinal_cost_file="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv"
+#source_directory="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/source_directory/"
 # Move Cardinal cost file to source directory
-echo "Moving Cardinal cost file to source directory..."
-mv "/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv" "$source_directory"
+#echo "Moving Cardinal cost file to source directory..."
+#mv "/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv" "$source_directory"
 # Set database connection details
 central_db_host="localhost"
 central_db_user="root"
@@ -15,7 +15,7 @@ central_table_name="Product_Cardinal"
 # Load product cost file into the EPRN Central database
 echo "Loading product cost file into the EPRN Central database..."
 mysql -h $central_db_host -u $central_db_user -p$central_db_password --local-infile=1 $central_db_name << EOF
-LOAD DATA INFILE '$source_directory/cardinal_cost_file.csv'
+LOAD DATA INFILE '$cardinal_cost_file'
 INTO TABLE $central_table_name
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
