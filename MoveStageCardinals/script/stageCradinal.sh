6a12845ad959e14c85d3acf52d3d97d5058d0d2b
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-17 12:51:09 +0530

    Update stageCradinal.sh

--- MoveStageCardinals/script/stageCradinal.sh
+++ MoveStageCardinals/script/stageCradinal.sh
@@ -10,23 +10,15 @@ central_db_host="localhost"
 central_db_user="root"
 central_db_password="priya"
 central_db_name="EPRN_Central "
+# Database table name
+central_table_name="Product_Cardinal"
 # Load product cost file into the EPRN Central database
 echo "Loading product cost file into the EPRN Central database..."
 mysql -h $central_db_host -u $central_db_user -p$central_db_password --local-infile=1 $central_db_name << EOF
 LOAD DATA INFILE '$source_directory/cardinal_cost_file.csv'
-INTO TABLE Product_Cardinal
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+INTO TABLE $central_table_name
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
-EOF
-store_db_host="localhost"
-store_db_user="root"
-store_db_password="priya"
-store_db_name="store_DB"
-# Push cost changes to the store database
-echo "Pushing cost changes to the store database..."
-mysql -h $store_db_host -u $store_db_user -p$store_db_password --local-infile=1 $store_db_name << EOF
-UPDATE Product_Store AS sp
-JOIN Product_Cardinal AS pc ON sp.Product_ID = pc.Product_ID
-SET sp.Price = pc.Price;
 EOF
 echo "Cost changes pushed to the store database."
