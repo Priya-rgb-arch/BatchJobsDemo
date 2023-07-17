8a4542297147fecd646553abfdb1b73d3e57f2ba
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-17 17:35:44 +0530

    Update stageCradinal.sh

--- MoveStageCardinals/script/stageCradinal.sh
+++ MoveStageCardinals/script/stageCradinal.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-
 # Database store connection details
 db_host="localhost"
 db_user="root"
 db_password="priya"
 db_store="EPRN_Central"
-
-
 # CSV file path and name
 csv_file="/home/sriharipriya/BatchJobsDemo/MoveStageCardinals/script/cardinal_cost_file.csv"
-
 # Database table name
 store_table_name="Product_Cardinal"
-
-
 # Read the CSV file and insert data into the database table
 mysql -h "$db_host" -u "$db_user" -p"$db_password" "$db_store" <<EOF
 LOAD DATA LOCAL INFILE '$csv_file'
@@ -22,7 +16,6 @@ FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 EOF
-
 if [ $? -eq 0 ]; then
   echo "Data inserted into the database table successfully."
 else
